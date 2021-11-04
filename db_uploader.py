3d7fd39c914f2cddd8d8d68a9f6dad552d64a701
hanasmile <hanasmile12@gmail.com>
2021-11-04 11:45:23 +0900

    Add: class Products 컬럼 추가

---
+++ db_uploader.py
@@ -0,0 +1,69 @@
+import os
+import django
+import csv
+import sys
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Wedibooks.settings")
+django.setup( )
+
+from products.models import Menu, Category, Subcategory, Product
+
+CSV_PATH_MENU = './menu.csv'
+CSV_PATH_CATEGORY = './category.csv'
+CSV_PATH_SUBCATEGORY = './subcategory.csv'
+CSV_PATH_PRODUCT = './product.csv'
+
+with open(CSV_PATH_MENU) as in_file:
+	data_reader = csv.reader(in_file)
+	next(data_reader, None)
+	for row in data_reader:
+		menu_name = row[0]
+		Menu.objects.create(name = menu_name)
+
+with open(CSV_PATH_CATEGORY) as in_file:
+    data_reader = csv.reader(in_file)
+    next(data_reader, None)
+    for row in data_reader:
+        category_name = row[0]
+        category_menu = row[1]
+
+        Category.objects.create(name = category_name, menu_id = category_menu)
+
+
+with open(CSV_PATH_SUBCATEGORY) as in_file:
+    data_reader = csv.reader(in_file)
+    next(data_reader, None)
+    for row in data_reader:
+        subcategory_name = row[0]
+        subcategory_category = row[1]
+
+        Subcategory.objects.create(name = subcategory_name, category_id = subcategory_category)
+
+with open(CSV_PATH_PRODUCT) as in_file:
+    data_reader = csv.reader(in_file)
+    next(data_reader, None)
+    for row in data_reader:
+        product_name = row[0]
+        product_author = row[1]
+        product_publisher = row[2]
+        product_date_published = row[3]
+        product_price = row[4]
+        product_description = row[5]
+        product_index = row[6]
+        product_thumbnail_image_url = row[7]
+        product_translator = row[8]
+        product_painter = row[9]
+        product_subcategory = row[10]
+
+        Product.objects.create(
+            name = product_name,
+            author = product_author,
+            publisher = product_publisher,
+            date_published = product_date_published,
+            price = product_price,
+            description = product_description,
+            index = product_index,
+            thumbnail_image_url = product_thumbnail_image_url,
+            translator = product_translator,
+            painter = product_painter,
+            subcategory_id = product_subcategory)

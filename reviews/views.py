15f9d5fd4c6436a6208b12e97df66f93f8fbd205
hanasmile <hanasmile12@gmail.com>
2021-11-11 16:21:18 +0900

    Add: Validation_Error 추가

--- reviews/views.py
+++ reviews/views.py
@@ -11,17 +11,20 @@ class ReviewView(View):
     def post(self,request):
         try:
             data    = json.loads(request.body)
-            product = data['product']
+            product_id = data['product_id']
             rating  = data['rating']
             content = data['content']
             
             Review.objects.create(
                 user_id    = request.user.id,
-                product_id = product,
+                product_id = product_id,
                 rating     = rating,
                 content    = content
             )
             return JsonResponse({"message": "SUCCESS"}, status=201)
+
+        except ValueError:
+            return JsonResponse({"message": "Validation_Error"}, status=405)
 
         except KeyError:
             return JsonResponse({"message": "Key_Error"}, status=400)

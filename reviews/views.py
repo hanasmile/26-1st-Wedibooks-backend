e082592a960fad30f2e40ea45fcf67fcd124f894
hanasmile <hanasmile12@gmail.com>
2021-11-11 15:29:47 +0900

    Modify: 띄어쓰기 수정

--- reviews/views.py
+++ reviews/views.py
@@ -10,18 +10,18 @@ class ReviewView(View):
     @login_required
     def post(self,request):
         try:
-            data = json.loads(request.body)
+            data    = json.loads(request.body)
             product = data['product']
-            rating = data['rating']
+            rating  = data['rating']
             content = data['content']
             
             Review.objects.create(
-                user_id = request.user.id,
+                user_id    = request.user.id,
                 product_id = product,
-                rating = rating,
-                content = content
+                rating     = rating,
+                content    = content
             )
             return JsonResponse({"message": "SUCCESS"}, status=201)
-            
+
         except KeyError:
             return JsonResponse({"message": "Key_Error"}, status=400)

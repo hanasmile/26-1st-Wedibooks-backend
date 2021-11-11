6a58f10be1f70d91959baf12e9d4b8aa5e2dd5c4
hanasmile <hanasmile12@gmail.com>
2021-11-11 15:23:30 +0900

    Add: ReviewView

---
+++ reviews/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+from .views import ReviewView
+
+urlpatterns = [
+	path('', ReviewView.as_view()),
+]
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from users.utils import login_required
+from .models import Review
+
+class ReviewView(View):
+    @login_required
+    def post(self,request):
+        try:
+            data = json.loads(request.body)
+            product = data['product']
+            rating = data['rating']
+            content = data['content']
+            
+            Review.objects.create(
+                user_id = request.user.id,
+                product_id = product,
+                rating = rating,
+                content = content
+            )
+            return JsonResponse({"message": "SUCCESS"}, status=201)
+            
+        except KeyError:
+            return JsonResponse({"message": "Key_Error"}, status=400)
